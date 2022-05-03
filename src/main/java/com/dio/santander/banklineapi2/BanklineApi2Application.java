8f2f352b3591644bd0f1f665b3311c16f883d767
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-03 15:22:14 -0300

    Subindo o projeto spring boot

---
+++ src/main/java/com/dio/santander/banklineapi2/BanklineApi2Application.java
@@ -0,0 +1,13 @@
+package com.dio.santander.banklineapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApi2Application.class, args);
+	}
+
+}
