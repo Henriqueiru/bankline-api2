64e8e11047798ba146be47b80c0048af9c9227b2
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-03 15:23:34 -0300

    alterando arquivo principal

--- src/main/java/com/dio/santander/banklineapi2/BanklineApi2Application.java
+++ src/main/java/com/dio/santander/banklineapi2/BanklineApi2Application.java
@@ -8,6 +8,7 @@ public class BanklineApi2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApi2Application.class, args);
+		//iremos comecar a alterar o projeto
 	}
 
 }
